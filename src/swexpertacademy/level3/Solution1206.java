3a438f26e6d017a6d048a2b6b8d736d36092c2ba
somsom <some1171@naver.com>
2019-02-24 14:26:29 +0900

    swexpertacademy level3 1206, 2805

---
+++ src/swexpertacademy/level3/Solution1206.java
@@ -0,0 +1,39 @@
+package swexpertacademy.level3;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution1206 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = 0, N = 0, A = 0;
+		int leftView, rightView;
+		int[] buildings = null;
+		String[] temp = null;
+		
+		while(N < 10) {
+			A = 0;
+			T = Integer.parseInt(in.readLine());
+			temp = in.readLine().split(" ");
+			buildings = new int[T];
+			
+			for(int i = 0; i < temp.length; i++) {
+				buildings[i] = Integer.parseInt(temp[i]);
+			}
+			
+			for(int i = 2; i < buildings.length - 2; i++) {
+				leftView = buildings[i] - Math.max(buildings[i-1], buildings[i-2]);
+				rightView = buildings[i] - Math.max(buildings[i+1], buildings[i+2]);
+
+				if (leftView > 0 && rightView > 0) {
+					A += Math.min(leftView, rightView);
+				}
+			}
+			
+			System.out.println("#" + (N+1) + " " + A);
+			N++;
+		}
+	}
+}
+
---
+++ src/swexpertacademy/level3/Solution2805.java
@@ -0,0 +1,49 @@
+package swexpertacademy.level3;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution2805 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int totalTestCaseCount, farmSize, centerValue, benefit;
+        String[] temp;
+        int[] farmLine;
+
+        totalTestCaseCount = Integer.parseInt(in.readLine());
+
+        for (int k = 0; k < totalTestCaseCount; k++) {
+            benefit = 0;
+            farmSize = Integer.parseInt(in.readLine());
+            centerValue = (farmSize - 1) / 2;
+            farmLine = new int[farmSize];
+
+            for (int i = 0; i < (farmSize + 1) / 2; i++) {
+                temp = in.readLine().split("");
+
+                for (int j = 0; j < farmSize; j++) {
+                    farmLine[j] = Integer.parseInt(temp[j]);
+                }
+
+                for (int j = centerValue - i; j < centerValue + i + 1; j++) {
+                    benefit += farmLine[j];
+                }
+            }
+
+            for (int i = (farmSize - 1) / 2 - 1; i >= 0; i--) {
+                temp = in.readLine().split("");
+
+                for (int j = 0; j < farmSize; j++) {
+                    farmLine[j] = Integer.parseInt(temp[j]);
+                }
+
+                for (int j = centerValue - i; j < centerValue + i + 1; j++) {
+                    benefit += farmLine[j];
+                }
+            }
+
+            System.out.printf("#%d %d\n", (k + 1), benefit);
+        }
+    }
+}
