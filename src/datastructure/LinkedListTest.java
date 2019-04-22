05b08c8e2fa99afec98ee2048a6d6db4a47a3a25
gthoya <gthoya85@hanmail.net>
2019-04-22 20:43:26 +0900

    [2019-04-22] linked list 버그 수정 사항 반영

--- src/datastructure/LinkedListTest.java
+++ src/datastructure/LinkedListTest.java
@@ -3,45 +3,40 @@ package datastructure;
 public class LinkedListTest {
 	public static void main(String[] args) {
 		LinkedList ll = new LinkedList();
-		
+
 		ll.append(1);
 		ll.append(2);
 		ll.append(3);
 		ll.append(4);
 		ll.print();
-		
-		ll.delete(3);
+
 		ll.print();
+
 	}
 }
 
 class LinkedList {
 	Node header;
-	
-	static class Node {
-		int data;
-		Node next = null;
-	}
-	
+
 	LinkedList() {
 		header = new Node();
 	}
-	
+
 	void append(int data) {
 		Node end = new Node();
 		end.data = data;
 		Node node = header;
-		
+
 		while (node.next != null) {
 			node = node.next;
 		}
-		
+
 		node.next = end;
 	}
-	
+
 	void delete(int data) {
 		Node node = header;
-		
+
 		while (node.next != null) {
 			if (node.next.data == data) {
 				node.next = node.next.next;
@@ -50,14 +45,84 @@ class LinkedList {
 			}
 		}
 	}
-	
-	void print() {
+
+	void deleteAt(int index) {
 		Node node = header.next;
 		
+		if (index < 0 || index > this.size() - 1) {
+			System.out.println("Incorrect Index!");
+			
+			return;
+		}
+		
+		for (int i = 0; i <= index; i++) {
+			if (index == this.size() - 1 && i == this.size() - 2) {
+				node.next = null;
+				
+				return;
+			} else if (index == 0) {
+				node = node.next;
+				header.next = node;
+				
+				return;
+			}
+			
+			node = node.next;
+		}
+		
+		node.next = node.next.next;
+	}
+
+	void deleteAll() {
+		header.next = null;
+	}
+
+	int get(int index) {
+		
+		if (index < 0 || index > this.size() - 1) {
+			System.out.println("Incorrect Index!");
+			
+			return 0;
+		}
+		
+		Node node = header.next;
+
+		for (int i = 0; i < index; i++) {
+			node = node.next;
+		}
+
+		return node.data;
+	}
+	
+	int size() {
+		int size = 1;
+		Node node = header.next;
+		
+		while (node.next != null) {
+			node = node.next;
+			size++;
+		}
+		
+		return size;
+	}
+
+	void print() {
+		Node node = header.next;
+
+		if (node == null) {
+			return;
+		}
+
 		while (node.next != null) {
 			System.out.print(node.data + " -> ");
 			node = node.next;
 		}
+
 		System.out.println(node.data);
 	}
+}
+
+class Node {
+	int data;
+	Node next = null;
 }
