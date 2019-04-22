a50aea26185cb99e2375d81e7e6a860b7e5de7cd
somsom <some1171@naver.com>
2019-04-22 16:25:59 +0900

    2019/04/22 data structure, linked list

---
+++ src/datastructure/LinkedListTest.java
@@ -0,0 +1,63 @@
+package datastructure;
+
+public class LinkedListTest {
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList();
+		
+		ll.append(1);
+		ll.append(2);
+		ll.append(3);
+		ll.append(4);
+		ll.print();
+		
+		ll.delete(3);
+		ll.print();
+	}
+}
+
+class LinkedList {
+	Node header;
+	
+	static class Node {
+		int data;
+		Node next = null;
+	}
+	
+	LinkedList() {
+		header = new Node();
+	}
+	
+	void append(int data) {
+		Node end = new Node();
+		end.data = data;
+		Node node = header;
+		
+		while (node.next != null) {
+			node = node.next;
+		}
+		
+		node.next = end;
+	}
+	
+	void delete(int data) {
+		Node node = header;
+		
+		while (node.next != null) {
+			if (node.next.data == data) {
+				node.next = node.next.next;
+			} else {
+				node = node.next;
+			}
+		}
+	}
+	
+	void print() {
+		Node node = header.next;
+		
+		while (node.next != null) {
+			System.out.print(node.data + " -> ");
+			node = node.next;
+		}
+		System.out.println(node.data);
+	}
+}
