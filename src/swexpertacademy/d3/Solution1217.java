0036d594658a0dcfc2704e68cc36773e6f3aa924
somsom <some1171@naver.com>
2019-05-27 13:45:47 +0900

    [2019-05-27] solution 1217 initial commit

---
+++ src/swexpertacademy/d3/Solution1217.java
@@ -0,0 +1,31 @@
+package swexpertacademy.d3;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution1217 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int count = 0;
+		
+		while (count < 10) {
+			int testCase = in.nextInt();
+			int number = in.nextInt();
+			int power = in.nextInt();
+			int answer = 1;
+			
+			Stack<Integer> stack = new Stack<Integer>();
+			
+			for (int i = 0; i < power; i++) {
+				stack.push(number);
+			}
+			
+			while (!stack.isEmpty()) {
+				answer *= stack.pop();
+			}
+			
+			System.out.println("#" + testCase + " " + answer);
+			count++;
+		}
+	}
+}
